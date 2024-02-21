2ee93920ff91ee703ed84d91ddd602045b876d90
rk1942 <rishabhkarthikeya@gmail.com>
2024-02-21 14:49:28 +0530

    changes

---
+++ hof/src/App.jsx
@@ -0,0 +1,11 @@
+import HigherOrderComponent from './components/HigherOrderComponent';
+import React from 'react';
+function App() {
+  return (
+    <>
+      <HigherOrderComponent />
+    </>
+  );
+}
+
+export default App;
